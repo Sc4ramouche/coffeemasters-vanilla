fafff2e4ab541d81da9ea1d370c47e17d69a768a
Vladislav Kovechenkov <vladislav.kovechenkov@gmail.com>
2023-10-04 22:55:29 +0200

    Setting up Custom Components via Shadow DOM

---
+++ components/DetailsPage.js
@@ -0,0 +1,12 @@
+export class DetailsPage extends HTMLElement {
+  constructor() {
+    super();
+    this.root = this.attachShadow({ mode: "open" });
+  }
+  connectedCallback() {
+    const template = document.getElementById("details-page-template");
+    const content = template.content.cloneNode(true);
+    this.root.appendChild(content);
+  }
+}
+customElements.define("details-page", DetailsPage);
