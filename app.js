7ec659160ad2d89bff74af26efcda43008c85e51
Vladislav Kovechenkov <vladislav.kovechenkov@gmail.com>
2023-10-05 22:56:19 +0200

    render list of products and product details components

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import Store from "./services/Store.js";
 import { MenuPage } from "./components/MenuPage.js";
 import { DetailsPage } from "./components/DetailsPage.js";
 import { OrderPage } from "./components/OrderPage.js";
+import ProductItem from "./components/ProductItem.js";
 
 window.app = {};
 app.store = Store;
