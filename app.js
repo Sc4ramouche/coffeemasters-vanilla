64592d40ecc7aad2e515e4bbc89f55371b891f2e
Vladislav Kovechenkov <vladislav.kovechenkov@gmail.com>
2023-10-06 22:45:43 +0200

    finishing with order page

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ import { MenuPage } from "./components/MenuPage.js";
 import { DetailsPage } from "./components/DetailsPage.js";
 import { OrderPage } from "./components/OrderPage.js";
 import ProductItem from "./components/ProductItem.js";
+import CartItem from "./components/CartItem.js";
 
 window.app = {};
 app.store = Store;
@@ -14,4 +15,11 @@ app.router = Router;
 window.addEventListener("DOMContentLoaded", async () => {
   loadData();
   app.router.init();
+});
+
+window.addEventListener("appcartchange", () => {
+  const badge = document.getElementById("badge");
+  const qty = app.store.cart.reduce((acc, p) => acc + p.quantity, 0);
+  badge.textContent = qty;
+  badge.hidden = qty == 0;
 });
